93425e4026e12592c73267947bbcebb06d3c5b3b
Lilanga Gamage <lilanga@live.com>
2023-09-26 12:34:50 +1000

    moved to functional component

--- src/components/check_connection.js
+++ src/components/check_connection.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class CheckConnection extends Component {
-  render() {
+export default function CheckConnection() {
     return (
       <div className="no-connection">
         <strong>{ remote.getGlobal('calendarName') }</strong>
         <div className="icon"><i className="icon icon-no-connection"></i></div>
-        <h3 className="text">Failed to connect to the Google Calendar API</h3>
+        <h3 className="text">Failed to connect to the Calendar Service</h3>
       </div>
     )
-  }
 }
