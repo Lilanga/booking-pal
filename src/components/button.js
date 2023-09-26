7f27f56cc8d9d0889de48c1c70d76f8b01c7ee94
Lilanga Gamage <lilanga@live.com>
2023-09-26 12:29:56 +1000

    moved to functional component

--- src/components/button.js
+++ src/components/button.js
@@ -1,50 +1,49 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import PropTypes, { func } from 'prop-types';
 import classNames from 'classnames';
 
-export default class Button extends Component {
-  static propTypes = {
-    icon: PropTypes.string.isRequired,
-    handleClick: PropTypes.func.isRequired,
-    disabled: PropTypes.bool
-  }
+function Button(props) {
 
-  static defaultProps = {
-    disabled: false
-  }
+  const [clicked, setClicked] = useState(false);
+  let clickedTimer;
 
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = {
-      clicked: false,
-    };
-  }
-
-  handleClick(e) {
-    this.setState({ clicked: true });
-    this.clickedTimer = setTimeout(
-      () => this.setState({ clicked: false }),
+  function handleClick(e) {
+    setClicked(true);
+    clickedTimer = setTimeout(
+      () => setClicked(false),
       1000
     );
-    this.props.handleClick(e);
+
+    props.handleClick(e);
   }
 
-  componentWillUnmount() {
-    clearTimeout(this.clickedTimer);
-  }
+  useEffect(() => {
+    return () => {
+      clearTimeout(clickedTimer);
+    }
+  }, []);
 
-  render() {
-    const iconClasses = classNames('icon', `icon-${this.props.icon}`);
-    const btnClasses = classNames({
-      clicked: this.state.clicked,
-      disabled: this.props.disabled
-      }, this.props.className);
+  const iconClasses = classNames('icon', `icon-${props.icon}`);
+  const btnClasses = classNames({
+    clicked: clicked,
+    disabled: props.disabled
+  }, props.className);
 
-    return (
-      <button onClick={this.handleClick} className={btnClasses} disabled={this.state.clicked}>
-        <i className={iconClasses}></i>
-      </button>
-    );
-  }
+  return (
+    <button onClick={handleClick} className={btnClasses} disabled={clicked}>
+      <i className={iconClasses}></i>
+    </button>
+  );
 }
+
+Button.propTypes = {
+  icon: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool
+}
+
+Button.defaultProps = {
+  disabled: false
+}
+
+export default Button;
