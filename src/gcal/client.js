534028530fa6a35baec94b9e5aead79f280b3302
Lilanga Gamage <lilanga@live.com>
2023-11-09 11:45:20 +1100

    refactor: change to use service account for google api authentication

--- src/gcal/client.js
+++ src/gcal/client.js
@@ -1,7 +1,7 @@
 const { google } = require('googleapis');
 
 let _auth, _calendarId;
-const calendar = google.calendar('v3');
+let calendar = google.calendar('v3');
 
 const MILLISECONDS_PER_MINUTE = 60000;
 
@@ -9,6 +9,7 @@ module.exports = class Client {
   constructor(calendarId, auth) {
     _calendarId = calendarId,
     _auth = auth;
+    calendar = google.calendar({version: "v3", auth:_auth});
   }
 
   listEvents() {
@@ -16,7 +17,6 @@ module.exports = class Client {
     const timeMax = new Date(new Date().setHours(23, 59, 59)).toISOString();
     return new Promise((resolve, reject) => {
       calendar.events.list({
-        auth: _auth,
         calendarId: _calendarId,
         timeMin,
         timeMax,
@@ -46,7 +46,6 @@ module.exports = class Client {
         },
       };
       calendar.events.insert({
-        auth: _auth,
         calendarId: _calendarId,
         resource,
       }, (err) => {
@@ -68,7 +67,6 @@ module.exports = class Client {
         },
       };
       calendar.events.patch({
-        auth: _auth,
         calendarId: _calendarId,
         eventId,
         resource,
